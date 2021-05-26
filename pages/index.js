f524b9a39d8bee33b9d3bf5f47e21934354049ac
natekcroteau <natekcroteau@gmail.com>
2021-05-26 11:13:58 -0600

    wireframed and created outline

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,30 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+      
+        <div>
+          <h1>Nate Croteau</h1>
+
+          <nav> About Me | Projects | Blog </nav>
+        </div>
+
+
+        <div>
+          <h1>About Me</h1>
+        </div>
+
+
+        <div>
+          <h1>Projects</h1>
+        </div>
+
+
+        <div>
+          <h1>Blog</h1>
+        </div>
+
 
       </main>
-
 
     </div>
   )
